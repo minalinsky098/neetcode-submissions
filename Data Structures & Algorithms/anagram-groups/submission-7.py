c8247a57c50a8ffcf4df0892a7e700b45b12691d
Royce Albolote <121223142+minalinsky098@users.noreply.github.com>
2026-08-05 18:08:53 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = dict()
+        anagrams = []
+        hashlist = []
+        for i in strs:
+            hashmap = {}
+            for j in i:
+                if j not in hashmap:
+                    hashmap[j] = 1
+                else:
+                    hashmap[j] += 1
+            if hashmap in hashlist:
+                anagrams[hashlist.index(hashmap)].append(i)
+                continue
+            else:
+                hashlist.append(hashmap)
+            anagrams.append([i])
+        return anagrams
