f81643fd45b678be68bf363bcc15c2716ef5a59e
Royce Albolote <121223142+minalinsky098@users.noreply.github.com>
2026-08-20 18:38:07 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        s = s.upper()
+        for i in range(len(s)):
+            if not s[left].isalnum():
+                print("moving left")
+                left += 1
+                continue
+            if not s[right].isalnum():
+                print("moving right")
+                right -= 1
+                continue
+            print(s[left], s[right])
+            if s[left] != s[right]:
+                return False
+            elif left == right:
+                return True
+            left += 1
+            right -= 1
+        return True
